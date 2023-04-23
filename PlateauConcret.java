7b5e7a54014314ab6ae8a1b49595c8e312e4e799
ranDemoulin <marchombre@DESKTOP-JA5LINK4.localdomain>
2023-04-23 19:44:24 +0200

    init commit

--- PlateauConcret.java
+++ PlateauConcret.java
@@ -4,8 +4,9 @@ import Plateau.PlateauAbstrait;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.Writer;
+import java.util.Scanner;
 import java.util.Stack;
+import java.io.FileNotFoundException;
 import java.security.InvalidParameterException;
 
 public class PlateauConcret extends PlateauAbstrait {
@@ -23,8 +24,11 @@ public class PlateauConcret extends PlateauAbstrait {
 
 		@Override
 		public String toString() { return val + " " + x + " " + y ; }
+
 		public int get_val(){ return val; }
+
 		public int get_x(){ return x; }
+
 		public int get_y(){ return y; }
 	}
 
@@ -32,7 +36,25 @@ public class PlateauConcret extends PlateauAbstrait {
 		super(i,j);
 	}
 
-	public PlateauConcret(String fichier) {
+	public PlateauConcret(String fichier) throws FileNotFoundException {
+		// Init fichier
+		Scanner sc_f = new Scanner(new File(fichier));
+
+		// Init var
+		int val, x, y;
+
+		// Tant que le fichier n'est pas vide, vérifie que le fichier est comforme et joue si il trouve un coup
+		while(sc_f.hasNext()){
+			try {
+				val = sc_f.nextInt();
+				x = sc_f.nextInt();
+				y = sc_f.nextInt();
+				joue(val, x, y);
+			}catch (Exception E) {
+				System.out.println(fichier + " isn't a save file");
+			}
+		}
+
 	}
 
 	@Override
@@ -40,8 +62,9 @@ public class PlateauConcret extends PlateauAbstrait {
 		if (coupJouable(valeur, i, j)) {
 			fixeValeurCase(valeur, i, j);
 			pil_passer.push(new COUP(valeur, i, j));
-			while(!pil_futur.empty())
+			while(!pil_futur.empty()) {
 				pil_futur.pop();
+			}
 		}else
 			throw new InvalidParameterException("Joue " + valeur + " en (" + i + ", " + j + ")");
 	}
@@ -84,16 +107,20 @@ public class PlateauConcret extends PlateauAbstrait {
 		File f = new File(fichier);
 		FileWriter w_f = new FileWriter(f);
 
+		// Init var
 		String sauv_data = "";
 		COUP c;
 
+		// On vide la pile des coup dans pil_futur pour commencer par ecrire le premier coup dans le fichier
 		while (!pil_passer.empty()) {
-			pil_futur.push(pil_passer.pop());// On vide la pile des coup dans pil_futur
-		}                                     // pour commencer par ecrire le premier coup dans le fichier
+			pil_futur.push(pil_passer.pop());
+		}
+
+		//On creer le String de data de sauvegarde (i.e la liste des coup réaliser)
 		while(!pil_futur.empty()){
 			c = pil_futur.pop();
-			sauv_data += c.toString();
+			sauv_data += c.toString() + " ";
 		}
-		w_f.write();
+		w_f.write(sauv_data);
 	}
 }
